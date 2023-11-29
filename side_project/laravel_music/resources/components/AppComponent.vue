66834d358c3ace7ff209a652d07413aaee3c6d28
yacci5513 <ckstjddh1@naver.com>
2023-11-29 10:18:12 +0900

    231129 npm install 추가

---
+++ side_project/laravel_music/resources/components/AppComponent.vue
@@ -0,0 +1,30 @@
+<template>
+	
+	
+</template>
+<script>
+export default {
+	name: 'AppComponent',
+	props: {
+
+	},
+	
+	components: {
+
+	},
+
+	data() {
+		return {
+			setting: '',
+		}
+	},
+
+	created() {},
+
+	mounted() {},
+
+	methods: {
+
+	}
+}
+</script>
